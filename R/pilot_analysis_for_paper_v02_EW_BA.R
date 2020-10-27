a964b17cfb4c10869bfeb43e6b0c691128a77beb
Ben Anderson <dataknut@icloud.com>
2020-10-27 20:33:53 +0000

    add a .Rmd version of the consent models exploration code for clarity. Includes potentially useful table for the paper and also expands on the plotting method of displaying results. IMD effect is retained and Cell 2 looks interesting but only cell 8 is sig at 95%... See output and /plots for details.

--- R/pilot_analysis_for_paper_v02_EW_BA.R
+++ R/pilot_analysis_for_paper_v02_EW_BA.R
@@ -37,7 +37,7 @@ gmodels::CrossTable(all_onboards$region, all_onboards$quintile,
            dnn = c("Outcome","IMD quintile") )#how do we turn off observed frequencies?
            
            
-# simple consent model
+# Simple consent model (full data as loaded) ----
 table(all_onboards$consent, useNA = "always") # check for NA
 prop.table(table(all_onboards$consent, useNA = "always")) # check for NA
 nonResponseModel <- glm(formula = consent ~ as.factor(quintile) + 
@@ -45,12 +45,16 @@ nonResponseModel <- glm(formula = consent ~ as.factor(quintile) +
                        family = binomial(logit), all_onboards) # wiil auto-drop the missing values
 
 summary(nonResponseModel)
+# CIs for coef not OR for plot
+coefCI <- cbind(coef = coef(nonResponseModel), 
+                confint(nonResponseModel))
+library(broom) # tidy up regression outputs
+df <- broom::tidy(nonResponseModel)
+df$model <- "2. Original"
 
-# CIs
-coefCI <- exp(cbind(OR = coef(nonResponseModel), confint(nonResponseModel)))
+originalDT <- data.table::as.data.table(cbind(coefCI, df)) # collect for later plot
 
-# Diagnostics:  ----
-# Independence of errors
+# > Diagnostics: Independence of errors ----
 car::durbinWatsonTest(nonResponseModel)
 # Andy Field DSUR:The test statistic can vary between 0 and 4 with a value of 2 meaning that the 
 # residuals are uncorrelated. A value greater than 2 indicates a negative correlation between 
@@ -68,8 +72,10 @@ head(all_onboards)
 # hmm
 
 # these results suggest a problem with the model but I suspect this is caused by
-# a) the data being orded by the outcome (consent = T/F)
+# a) the data being ordered by the outcome (consent = T/F)
 # b) large sample size -> so p value is going to be small...
+
+# Simple consent model (randomly sampled) ----
 
 # Create a smaller randomly ordered sample by randomly selecting 3000
 # with replacement
@@ -86,17 +92,24 @@ nonResponseModelReduced <- glm(formula = consent ~ as.factor(quintile) +
                         family = binomial(logit), dt) # wiil auto-drop the missing values
 
 summary(nonResponseModelReduced)
-# CIs
-coefCI <- exp(cbind(OR = coef(nonResponseModelReduced), 
-                    confint(nonResponseModelReduced)))
 
-# Diagnostics:  ----
-# Independence of errors
+# CIs for coef not OR for plot
+coefCI <- cbind(coef = coef(nonResponseModelReduced), 
+                confint(nonResponseModelReduced))
+
+df <- broom::tidy(nonResponseModelReduced)
+df$model <- "1: Reduced"
+
+reducedDT <- data.table::as.data.table(cbind(coefCI, df)) # collect for later plot
+
+# > Diagnostics: Independence of errors ----
 car::durbinWatsonTest(nonResponseModelReduced)
 
 # plot to confirm
 plot(nonResponseModelReduced$residuals)
 # noisy & random
+
+# Simple consent model (full data randomly shuffled) ----
 
 # OK, so let's shuffle the original data randomly
 all_onboards <- all_onboards[sample(nrow(all_onboards)),]
@@ -106,38 +119,42 @@ nonResponseModelShuffled <- glm(formula = consent ~ as.factor(quintile) +
                                  region, 
                                family = binomial(logit), all_onboards) # wiil auto-drop the missing values
 
-summary(nonResponseModelShuffled) # should be indentical to first model
+summary(nonResponseModelShuffled) # should be identical to first model
 # NB the model is not explaining a lot
 # This is kind of what we want - it suggests these area level indicators are
-# not strong predicters of consent
+# not strong predictors of consent
 
-# CIs for coef not OR
-coefCI <-cbind(coef = coef(nonResponseModelShuffled), 
-                    confint(nonResponseModelShuffled))
 
-library(broom)
+# CIs for coef not OR for plot
+coefCI <- cbind(coef = coef(nonResponseModelShuffled), 
+                confint(nonResponseModelShuffled))
+
 df <- broom::tidy(nonResponseModelShuffled)
+df$model <- "3: Shuffled"
 
-plotDT <- data.table::as.data.table(cbind(coefCI, df))
-data.table::setnames(plotDT, c("2.5 %","97.5 %"), c("lo", "up"))
+shuffledDT <- data.table::as.data.table(cbind(coefCI, df)) # collect for later plot
+
+plotDT <- rbind(originalDT, reducedDT, shuffledDT)
+data.table::setnames(plotDT, c("2.5 %","97.5 %"), c("lo", "up")) # for easier ggplotting
+
 
 # useful method of platting OR with CI
 # need to tidy the labels etc
 library(ggplot2)
-p <- ggplot2::ggplot(plotDT, aes(x = coef, y = term , fill = coef)) + 
-  geom_col() +
-  geom_errorbar(aes(xmax = up, xmin = lo)) +
+p <- ggplot2::ggplot(plotDT, aes(x = coef, y = term , fill = model)) + 
+  geom_col(position = "dodge") +
+  geom_errorbar(aes(xmax = up, xmin = lo),position = "dodge") +
   geom_vline(xintercept = 0) +
   theme(legend.position="bottom")
 p
-library(here)
-ggplot2::ggsave("nonResponseModelShuffled.png", plot = p,
+
+library(here) # so useful - relative repo paths!
+ggplot2::ggsave("nonResponseModelCompare.png", plot = p,
                 path = here::here("plots"))
 
-# the interceot effect is very large - so the model is not telling us a lot...
+# the intercept effect is very large - so the model is not telling us a lot...
 
-# Diagnostics:  ----
-# Independence of errors
+# > Diagnostics: Independence of errors ----
 car::durbinWatsonTest(nonResponseModelShuffled)
 # DW = 1.88, I suspect the p value = 0 due to sample size (NB also Note
 # in https://www.rdocumentation.org/packages/car/versions/3.0-10/topics/durbinWatsonTest
